078c98363bc41a02f8bb49d627386936344bd26e
Andrew Zhuravlev <frntxacc@gmail.com>
2019-08-23 19:55:31 +0300

    Changed from list of Vector2 to List of Nodes for performance and code cleanup

--- Assets/Scripts/PlatformerPathfinding/PathFindingAgent.cs
+++ Assets/Scripts/PlatformerPathfinding/PathFindingAgent.cs
@@ -14,7 +14,7 @@ public class PathFindingAgent : MonoBehaviour {
     public int Height => _height;
     public int Width => _width;
 
-    List<Vector2> _path;
+    List<Node> _path;
     
     void Update() {
         //if (Input.GetMouseButtonDown(0)) {
@@ -26,10 +26,10 @@ public class PathFindingAgent : MonoBehaviour {
         if (_path == null || _path.Count == 0)
             return;
         Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position, _path[0]);
+        Gizmos.DrawLine(transform.position, _path[0].WorldPosition);
         for (int i = 0; i < _path.Count - 1; i++) {
             Gizmos.color = Gizmos.color == Color.blue ? Color.magenta : Color.blue;
-            Gizmos.DrawLine(_path[i], _path[i + 1]);
+            Gizmos.DrawLine(_path[i].WorldPosition, _path[i + 1].WorldPosition);
         }
     }
 }
